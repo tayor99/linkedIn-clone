ac7948930608447936759990e58b82ec06b15d65
Adenusi Adetayo <adenusiadetayo@gmail.com>
2022-06-18 08:48:41 +0100

    completed header section

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../styles/Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1655533872~hmac=24b53f5bba7a88c03b05db8f2b9beac8"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notification" />
+        <HeaderOption
+          avatar="https://pbs.twimg.com/profile_images/1528624295479595010/SwapSZK9_400x400.jpg"
+          title="meee"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
